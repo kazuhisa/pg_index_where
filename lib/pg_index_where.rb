490fe02441bf59a16c0e7dea132ad9e64136078a
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2013-05-23 21:00:07 +0900

    added SchemaDumper

--- lib/pg_index_where.rb
+++ lib/pg_index_where.rb
@@ -1,4 +1,4 @@
-require "pg_index_where/version"
+require 'pg_index_where/version'
 
 module ActiveRecord
   module ConnectionAdapters
@@ -10,4 +10,54 @@ module ActiveRecord
       end
     end
   end
+
+  class SchemaDumper
+    private
+    def indexes(table, stream)
+      if (indexes = @connection.indexes(table)).any?
+        add_index_statements = indexes.map do |index|
+          statement_parts = [
+            ('add_index ' + remove_prefix_and_suffix(index.table).inspect),
+            index.columns.inspect,
+            (':name => ' + index.name.inspect),
+          ]
+          statement_parts << ':unique => true' if index.unique
+
+          index_lengths = (index.lengths || []).compact
+          statement_parts << (':length => ' + Hash[index.columns.zip(index.lengths)].inspect) unless index_lengths.empty?
+
+          index_orders = (index.orders || {})
+          statement_parts << (':order => ' + index.orders.inspect) unless index_orders.empty?
+
+          index_where = index_with_where(table, index.name)
+          statement_parts << (':where => ' + index_where.inspect) unless index_where.nil?
+
+          '  ' + statement_parts.join(', ')
+        end
+
+        stream.puts add_index_statements.sort.join("\n")
+        stream.puts
+      end
+    end
+
+    def index_with_where(table, name)
+      sql = <<-EOS
+        SELECT distinct i.relname, pg_get_indexdef(d.indexrelid)
+                   FROM pg_class t
+                   INNER JOIN pg_index d ON t.oid = d.indrelid
+                   INNER JOIN pg_class i ON d.indexrelid = i.oid
+                   WHERE i.relkind = 'i'
+                     AND d.indisprimary = 'f'
+                     AND t.relname = '#{table}'
+                     AND i.relnamespace IN (SELECT oid FROM pg_namespace WHERE nspname = ANY (current_schemas(false)) )
+                     AND i.relname = '#{name}'
+                  ORDER BY i.relname
+      EOS
+      query_result = @connection.exec_query(sql).first
+      return nil unless query_result
+      query_result['pg_get_indexdef'] =~ /WHERE \((.+)\)$/
+      $1
+    end
+  end
 end
+
